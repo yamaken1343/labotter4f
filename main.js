a47b59ef7ea6a58abb2ad0bbf494243d6a63fcc0
yamaken <yamaken1343@gmail.com>
2020-08-19 23:59:28 +0900

    logページと設定ページの作成

--- main.js
+++ main.js
@@ -28,25 +28,6 @@ function main() {
     document.getElementById("labTime").innerText = toTimeString(diff);
 }
 
-function toTimeString(second) {
-    return ("00" + Math.floor(second / 3600)).slice(-2) + ":"
-        + ("00" + Math.floor(second / 60)).slice(-2) + ":"
-        + ("00" + Math.floor(second % 60)).slice(-2);
-}
-
-function toDateTimeString(unixTime) {
-    const D = new Date(unixTime * 1000);
-    console.log(D)
-    const Y = D.getFullYear();
-    const M = D.getMonth() + 1;
-    const d = D.getDate();
-    const h = ("00" + D.getHours()).slice(-2);
-    const m = ("00" + D.getMinutes()).slice(-2);
-    const s = ("00" + D.getSeconds()).slice(-2);
-    console.log(Y + "/" + M + "/" + d + " " + h + ":" + m + ":" + s)
-    return Y + "/" + M + "/" + d + " " + h + ":" + m + ":" + s;
-}
-
 function init() {
     inTime = localStorage.getItem("labin");
     if (localStorage.getItem("log")) {
